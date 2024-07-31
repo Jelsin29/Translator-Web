8ec3d7cd27da8f4db73458a63988b5c7facbb2e7
Jelsin29 <jelsimsanchez@gmail.com>
2024-08-01 02:16:25 +0300

    First Demo functional of Gradio

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import gradio as gr
 
+def translator():
+    pass
+
+web = gr.Interface(   
+    fn=translator,
+    inputs=gr.Audio(
+        sources=["microphone"],
+        type="filepath",
+    ),
+    outputs=[],
+    title="Speech to Text Translator",
+    description="A speech to text translator that converts spoken words into text."
+)
+
+web.launch()
