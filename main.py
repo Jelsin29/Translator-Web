dff996e9cb440efa6035c53a80ce3dbf9dc9b89d
Jelsin29 <jelsimsanchez@gmail.com>
2024-08-01 03:34:36 +0300

    We are using whisper and translate, to aafter get the audio convert it in to a text and then translate it to the desired language.

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import gradio as gr
+import whisper
+from translate import Translator
 
-def translator():
-    pass
+def translator(audio_file):
+   #1. Transcrip text
+
+   # https://github.com/openai/whisper.git
+   # Alternetives https://assemblyai.com
+   # Whisper (https://whisper.ai)
+
+   try:
+        model = whisper.load_model("base")
+        result = model.transcribe(audio_file, language="Spanish")
+        transcription = result["text"]
+   except Exception as e:
+         raise gr.Error(
+              f"Se ha producido un error al transcribir el texto: {str(e)}")
+   
+   #2. Translate text
+   # https://pypi.org/project/translate/
+
+   en_transcription = Translator(to_lang="en").translate(transcription)
+
+   # 3. Generate translate audio
+   
 
 web = gr.Interface(   
     fn=translator,
